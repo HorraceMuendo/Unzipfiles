010842e2735ecaf17b0880eeef115ffa4ef7f379
HorraceMuendo <horracemuendo34@gmail.com>
2023-01-20 23:12:52 +0300

    done writing the unzip function

--- Util/util.go
+++ Util/util.go
@@ -3,6 +3,7 @@ package util
 import (
 	"archive/zip"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -35,15 +36,31 @@ func unzipSrc(source, destination string) error {
 
 func unzipFile(f *zip.File, destination string) {
 	filepath := filepath.Join(destination, f.Name)
-
+	// checking vulnerability to zip slip
 	if !strings.HasPrefix(filepath, filepath.Clean(destination)+string(os.PathListSeparator)) {
 		return fmt.Errorf("invalid file path: %s", filepath)
 	}
-
+	//creating a directory tree so that all unziped files match the tree iside zip
 	if f.FileInfo().IsDir() {
 		if err := os.MkdirAll(filepath, os.ModePerm); err != nil {
 			return err
 		}
 		return nil
 	}
+	//creating a destination for unzipped files
+	destinationFile, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+	//unzipping file contents
+	zippedFile, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer zippedFile.Close()
+	if _, err := io.Copy(destinationFile, zippedFile); err != nil {
+		return err
+	}
+	return nil
 }
