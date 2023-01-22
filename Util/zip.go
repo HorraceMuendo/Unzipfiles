71be2ebd7aa5708b48785e2d635b65e035582857
HorraceMuendo <horracemuendo34@gmail.com>
2023-01-22 23:40:46 +0300

    project restructure and completed zip func

--- Util/zip.go
+++ Util/zip.go
@@ -1,25 +1,67 @@
 package util
 
-import(
-	"fmt",
-	"os",
-
+import (
+	"archive/zip"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
 )
 
-var Filename string
+//"fmt",
 
-f, _:= os.Open(Filename)
+func zipFile(source, target string) error {
+	// create a zip fle and zip.Writer
+	f, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-var read := bufio.NewReader(f)
+	writer := zip.NewWriter(f)
+	defer writer.Close()
 
-data , _:= ioutil.ReadAll(read)
+	//go through all the files in the source
+	return filepath.Walk(source, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		//local file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+		//set compression
+		header.Method = zip.Deflate
 
-Filename= strings.Replace(Filaname,".text",".gz",-1)
+		//relative file path as header name
+		header.Name, err = filepath.Rel(filepath.Dir(source), path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			header.Name += "/"
+		}
 
-f, _ := os.Create(Filename)
+		headerWriter, err := writer.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
 
-w := gzip.NewWriter(f)
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		_, err = io.Copy(headerWriter, f)
+		return err
 
-w.Write(data)
+	})
 
-w.close()
+}
